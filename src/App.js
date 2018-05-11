7a697afb668911de7728bd17ddf2a1dca8813c02
Amanda Hinchman-Dominguez <amandahinchman@Amandas-MBP-2.attlocal.net>
2018-05-11 01:12:42 -0400

    Ya'll wanted to know what I was really like

---
+++ src/App.js
@@ -0,0 +1,115 @@
+import React, { Component } from 'react';
+import './App.css';
+import GuestList from './GuestList';
+
+class App extends Component {
+
+  state = {
+    isFiltered: false,
+    guests: [
+      {
+        name: 'Treasure',
+        isConfirmed: false,
+        isEditing: false
+      },
+      {
+        name: 'Nic',
+        isConfirmed: true,
+        isEditing: false
+      },
+      {
+        name: 'Matt K',
+        isConfirmed: true,
+        isEditing: true
+      }
+    ]
+  }
+
+  toggleGuestPropertyAt = (property, indexToChange) => 
+    this.setState({
+      guests: this.state.guests.map((guest, index) => {
+        if (index === indexToChange) {
+          return {
+            ...guest, //allows for flexibility
+            [property]: !guest[property]
+          }
+        }
+        return guest;
+      })
+    });
+
+  toggleConfirmationAt = index =>
+    this.toggleGuestPropertyAt("isConfirmed", index);
+
+  toggleEditingAt = index =>
+    this.toggleGuestPropertyAt("isEditing", index);
+
+  setNameAt = (name, indexToChange) => 
+  this.setState({
+    guests: this.state.guests.map((guest, index) => {
+      if (index === indexToChange) {
+        return {
+          ...guest, //allows for flexibility
+          name
+        }
+      }
+      return guest;
+    })
+  });
+
+  toggleFilter = () =>
+    this.setState({ isFiltered: !this.state.isFiltered })
+
+  getTotalInvited = () => this.state.guests.length; 
+  // getAttendingGuests = () =>
+  // getUnconfirmedGuests = () =>
+
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <h1>Watch my dog attempt to play with my bird</h1>
+          <p>Will she stay calm or will she panic</p>
+          <form>
+            <input type="text" value="Safia" placeholder="Invite Someone" />
+            <button type="submit" name="submit" value="submit">Submit</button>
+          </form>
+          <p>Look at her panicking</p>
+        </header>
+        <div className="main">
+          <div>
+            <h2>Invitees</h2>
+            <label>
+              <input 
+                type="checkbox" 
+                onChange={this.toggleFilter}
+                checked={this.state.isFiltered}/> Hide those who haven't responded
+            </label>
+          </div>
+          <table className='counter'>
+            <tbody>
+              <tr>
+                <td>Attending:</td>
+                <td>2</td>
+              </tr>
+              <tr>
+                <td>Total:</td>
+                <td>3</td>
+              </tr>
+            </tbody>
+          </table>
+
+          <GuestList 
+            guests={this.state.guests} 
+            toggleConfirmationAt={this.toggleConfirmationAt}
+            toggleEditingAt={this.toggleEditingAt}
+            setNameAt={this.setNameAt} 
+            isFiltered={this.state.isFiltered}
+            />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
